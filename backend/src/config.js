0c56d7b1a81a09accfa382140be05af528f4c73e
Netvisio <you@example.com>
2022-05-12 21:20:37 -0400

    updated

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const TOKENS_PER_MINT = 50; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-06-01T11:59:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-31T11:59:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-05-11T11:59:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xdAD1746335198B0dd1aA4EfeC54285b04C43C73F"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
