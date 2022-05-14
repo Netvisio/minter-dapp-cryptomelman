8d835a3890620df30e945cbf893bfec8ff707305
Netvisio <jordanhdez@icloud.com>
2022-05-14 12:06:15 -0400

    updated

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0xdAD1746335198B0dd1aA4EfeC54285b04C43C73F"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = ""; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xab5e8F9670045C7853bA5C1724C5b57F29893d1c"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
